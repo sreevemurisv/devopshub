133bbd9534d69b7d6dd12c273113086c4c01121f
sreevemurisv <sreevemurisv@gmail.com>
2026-08-26 18:57:13 +0530

    added folders

---
+++ create-structure.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+REPO_NAME="devopshub"
+
+echo "Creating DevOps documentation directory structure..."
+
+mkdir -p \
+Linux \
+App-Server-Web-Server \
+Bash-Scripting \
+Git-GitHub \
+AWS/{Cloud-Introduction,Amazon-EC2,VPC,VPC-Peering,IAM,S3,Route53-CDN,CloudWatch-CloudTrail,Auto-Scaling-Groups,Load-Balancers,RDS,Lambda,API-Gateway,KMS,SQS,Amazon-MQ,Secrets-Manager,WAF,Systems-Manager/Parameter-Store,AWS-Architecture} \
+Jenkins \
+Docker \
+ECS \
+Terraform \
+Kubernetes \
+ArgoCD \
+Jenkins-Pipeline \
+Prometheus-Grafana \
+Ansible
+
+echo "Creating README.md..."
+
